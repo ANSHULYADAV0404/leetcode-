d83e4fcfd83d63a8526149bb2c97f36b6d90aa52
ANSHULYADAV0404 <anshuly4dav4444@gmail.com>
2026-07-22 21:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (94.16%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,21 +1,32 @@
-// Map wala tarika (Sahi Syntax ke saath)
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        
-        // 1. Sabhi numbers ki frequency count karo
-        for (int num : nums) {
-            mp[num]++;
-        }
-        
-        // 2. Wo number dhundho jiski frequency 1 hai
-        for (int num : nums) {
-            if (mp[num] == 1) {
-                return num;
+    int singleNonDuplicate(vector<int>& A) {
+        int n = A.size();
+        if (n == 1)
+            return A[0];
+        int lp = 0, rp = n - 1, mid;
+        while (lp <= rp) {
+            mid = lp + (rp - lp) / 2;
+            if (mid == 0 && A[0] != A[1])
+                return A[0];
+            if (mid == n - 1 && A[n - 1] != A[n - 2])
+                return A[n - 1];
+            if (A[mid - 1] != A[mid] && A[mid + 1] != A[mid])
+                return A[mid];
+            if (mid % 2 == 0) {
+                if (A[mid - 1] == A[mid]) {
+                    rp = mid - 1;
+                } else {
+                    lp = mid + 1;
+                }
+            } else {
+                if (A[mid - 1] == A[mid]) {
+                    lp = mid + 1;
+                } else {
+                    rp = mid - 1;
+                }
             }
         }
-        
-        return -1;
+        return mid;
     }
 };
