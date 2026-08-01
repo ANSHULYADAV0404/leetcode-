6e732cf065d34cc490345af672005e03786337c7
ANSHULYADAV0404 <anshuly4dav4444@gmail.com>
2026-08-01 21:10:28 +0530

    Time: 207 ms (17.39%) | Memory: 9.7 MB (98.80%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isFreqSame(int Freq1[], int Freq2[]) {
+        for (int i = 0; i < 26; i++) {
+            if (Freq1[i] != Freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1[i] - 'a']++;
+        }
+        int windSize = s1.length();
+        for (int i = 0; i < s2.length(); i++) {
+            int windIdx = 0, idx = i;
+            int windFreq[26] = {0};
+            while (windIdx < windSize && idx < s2.length()) {
+                windFreq[s2[idx] - 'a']++;
+                windIdx++;
+                idx++;
+            }
+            if (isFreqSame(freq, windFreq)) {
+                return true;
+            }
+            // return false;
+        }
+        return false;
+    }
+};
